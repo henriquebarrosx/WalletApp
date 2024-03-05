2a7844ccafdd886664faa69a76d04138434448f1
Henrique Barros <henriquebarrosx@gmail.com>
2024-03-04 23:32:36 -0300

    Add validations and frequency fields

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -7,11 +7,45 @@ import type { CategoriesServiceGateway } from '@/services/categories/index.gatew
 
 const categoriesService = inject('CategoriesService') as CategoriesServiceGateway
 
-const categoryList = ref<CategoryList>(new CategoryList())
+const CENTURY_IN_MONTHS = 1200
+const amountField = ref("")
+const frequencyDayField = ref(1)
+const frequencyTimesField = ref(2)
 const isCategoriesVisivel = ref(false)
-const frequencyField = ref("0")
+const categoryList = ref<CategoryList>(new CategoryList())
 
-const isFrequencyTimesFieldVisible = computed(() => frequencyField.value === "custom")
+const formattedFrequencyDay = computed(() => {
+  if (frequencyDayField.value <= 0) return '1'
+  return frequencyDayField.value < 10 ? `0${frequencyDayField.value}` : frequencyDayField.value
+})
+
+const formatedFrequencyTimesText = computed(() => {
+  if (parseInt(String(frequencyTimesField.value)) === 0) return 'tempo indeterminado'
+  return `${frequencyTimesField.value < 10 ? `0${frequencyTimesField.value}` : frequencyTimesField.value} meses`
+})
+
+const frequencyTimesValidation = computed(() => {
+  const isBeteweenInvalidInterval = frequencyTimesField.value < 0 || frequencyTimesField.value > CENTURY_IN_MONTHS
+  return isBeteweenInvalidInterval ? 'Frequência permitida: [min: 0 | max: 1200].' : ''
+})
+
+const frequencyDayValidation = computed(() => {
+  const isBeteweenInvalidPeriod = frequencyDayField.value < 1 || frequencyDayField.value > 28
+  return isBeteweenInvalidPeriod ? 'A ocorrência só poderá ocorrer entre os dias 01 e 28 de cada mês.' : ''
+})
+
+function formatAmountToCurrency(): void {
+  if (!amountField.value) return
+  const stringToFloat = parseFloat(amountField.value.replace(",", "."))
+  const floatWithTwoDecimalPlaces = stringToFloat.toFixed(2)
+  const formatThousands = floatWithTwoDecimalPlaces.replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+  amountField.value = formatThousands
+}
+
+function formatFrequencyTimes(): void {
+  const isInvalidFrequencyTimes = frequencyTimesField.value > CENTURY_IN_MONTHS || frequencyTimesField.value < 0
+  frequencyTimesField.value = isInvalidFrequencyTimes ? 0 : frequencyTimesField.value
+}
 
 async function fetchCategories() {
   try {
@@ -47,7 +81,8 @@ function toggleCategoriesModal() {
 
         <div class="flex w-full">
           <span class="px-4 py-2 bg-slate-500 text-white rounded-l">R$</span>
-          <input id="amount" class="input-field rounded-r" type="number" placeholder="0.00" min="0" />
+          <input v-model="amountField" @change="formatAmountToCurrency" id="amount" class="input-field rounded-r"
+            type="text" placeholder="0.00" />
         </div>
       </fieldset>
 
@@ -71,9 +106,16 @@ function toggleCategoriesModal() {
       </fieldset>
 
       <fieldset class="flex flex-col w-full gap-2">
-        <label for="description" class="text-slate-500 font-medium">
-          Categoria
-        </label>
+        <div class="flex items-center justify-between">
+          <label for="description" class="text-slate-500 font-medium">
+            Categoria
+          </label>
+
+          <button type="button" @click="toggleCategoriesModal"
+            class="text-blue-500 hover:underline duration-200 ease-in-out">
+            Gerenciar
+          </button>
+        </div>
 
         <select @focus="fetchCategories" class="input-field rounded bg-white h-11">
           <option v-if="categoryList.isLoading" disabled>Carregando...</option>
@@ -82,38 +124,38 @@ function toggleCategoriesModal() {
             {{ category.name }}
           </option>
         </select>
-
-        <div class="flex justify-end">
-          <button type="button" @click="toggleCategoriesModal"
-            class="text-blue-500 hover:underline duration-200 ease-in-out">
-            Gerenciar
-          </button>
-        </div>
       </fieldset>
 
-      <fieldset class="flex flex-col w-full gap-2 mt-[-28px]">
+      <fieldset class="flex flex-col w-full gap-2">
         <label for="description" class="text-slate-500 font-medium">
-          Repetir
+          Recorrência
         </label>
 
-        <select v-model="frequencyField" id="frequency" class="input-field rounded bg-white h-11">
-          <option value="0">Sempre</option>
-          <option value="1">1 mês</option>
-          <option value="2">2 meses</option>
-          <option value="3">3 meses</option>
-          <option value="custom">Personalizar</option>
-        </select>
-      </fieldset>
+        <div class="flex gap-4">
+          <div class="flex w-full">
+            <input v-model="frequencyDayField" id="frequencyDay" placeholder="0" class="input-field rounded-l" />
+            <span class="px-4 py-2 bg-slate-500 text-white rounded-r">dia</span>
+          </div>
 
-      <fieldset v-if="isFrequencyTimesFieldVisible" class="flex flex-col w-full gap-2">
-        <label for="description" class="text-slate-500 font-medium">
-          Recorrência personalizada
-        </label>
-
-        <div class="flex w-full">
-          <input id="frequencyTimes" class="input-field rounded-l" type="number" placeholder="0" min="0" />
-          <span class="px-4 py-2 bg-slate-500 text-white rounded-r">meses</span>
+          <div class="flex w-full">
+            <input v-model="frequencyTimesField" id="frequencyTimes" placeholder="0" class="input-field rounded-l" />
+            <span class="px-4 py-2 bg-slate-500 text-white rounded-r">vezes</span>
+          </div>
         </div>
+
+        <span v-if="!!frequencyDayValidation" class="text-red-400">
+          {{ frequencyDayValidation }}
+        </span>
+
+        <span v-if="!!frequencyTimesValidation" class="text-red-400">
+          {{ frequencyTimesValidation }}
+        </span>
+
+        <small class="text-slate-400 mt-2">
+          A repetição ocorrerá todo dia <strong class="underline">{{ formattedFrequencyDay }}</strong> por
+          <strong class="underline">{{ formatedFrequencyTimesText }}</strong>. Uma notificação será enviada via WhatsApp
+          até 5 dias antes alertando o vencimento! Defina como 0, caso o prazo seja indeterminado.
+        </small>
       </fieldset>
 
       <button class="rounded p-4 mt-4 bg-[#3155d2] hover:opacity-50 duration-200 ease-in-out text-white">
